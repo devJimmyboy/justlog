7da0501d409cb6c1f2ad2818cbe06fb0607c8749
devJimmyboy <jimmy@jimmyboy.dev>
2022-09-21 02:37:30 -0500

    update pkg & get ready to eject

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import { useContext } from 'react';
-import ReactDOM from 'react-dom';
-import { QueryClientProvider } from 'react-query';
-import { Page } from './components/Page';
-import { StateProvider, store } from './store';
-import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React from 'react'
+import { useContext } from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from 'react-query'
+import { Page } from './components/Page'
+import { StateProvider, store } from './store'
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles'
 
 const pageTheme = createMuiTheme({
-	palette: {
-		type: 'dark'
-	},
-});
+  palette: {
+    type: 'dark',
+  },
+})
 
 function App() {
-	const { state } = useContext(store);
+  const { state } = useContext(store)
 
-	return <QueryClientProvider client={state.queryClient}>
-		<Page />
-	</QueryClientProvider>
+  return (
+    <QueryClientProvider client={state.queryClient}>
+      <Page />
+    </QueryClientProvider>
+  )
 }
 
-ReactDOM.render(
-	<React.StrictMode>
-		<StateProvider>
-			<ThemeProvider theme={pageTheme}>
-				<App />
-			</ThemeProvider>
-		</StateProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(
+  <React.StrictMode>
+    <StateProvider>
+      <ThemeProvider theme={pageTheme}>
+        <App />
+      </ThemeProvider>
+    </StateProvider>
+  </React.StrictMode>
+)
