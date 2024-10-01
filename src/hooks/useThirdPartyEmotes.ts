b6831e2fe0428e2a8a0d59a3958e4bc7dd135914
devJimmyboy <jimmy@jimmyboy.dev>
2024-10-01 05:41:50 -0500

    prioritize 7tv emotes over other providers

--- src/hooks/useThirdPartyEmotes.ts
+++ src/hooks/useThirdPartyEmotes.ts
@@ -1,20 +1,20 @@
-import { ThirdPartyEmote } from "../types/ThirdPartyEmote";
-import { use7tvChannelEmotes } from "./use7tvChannelEmotes";
-import { use7tvGlobalEmotes } from "./use7tvGlobalEmotes";
-import { useBttvChannelEmotes } from "./useBttvChannelEmotes";
-import { useBttvGlobalEmotes } from "./useBttvGlobalEmotes";
-import { useFfzChannelEmotes } from "./useFfzChannelEmotes";
-import { useFfzGlobalEmotes } from "./useFfzGlobalEmotes";
+import { ThirdPartyEmote } from '../types/ThirdPartyEmote'
+import { use7tvChannelEmotes } from './use7tvChannelEmotes'
+import { use7tvGlobalEmotes } from './use7tvGlobalEmotes'
+import { useBttvChannelEmotes } from './useBttvChannelEmotes'
+import { useBttvGlobalEmotes } from './useBttvGlobalEmotes'
+import { useFfzChannelEmotes } from './useFfzChannelEmotes'
+import { useFfzGlobalEmotes } from './useFfzGlobalEmotes'
 
 export function useThirdPartyEmotes(channelId: string): Array<ThirdPartyEmote> {
-	const thirdPartyEmotes: Array<ThirdPartyEmote> = [
-		...useBttvChannelEmotes(channelId),
-		...useFfzChannelEmotes(channelId),
-		...use7tvChannelEmotes(channelId),
-		...useBttvGlobalEmotes(),
-		...useFfzGlobalEmotes(),
-		...use7tvGlobalEmotes(),
-	];
+  const thirdPartyEmotes: Array<ThirdPartyEmote> = [
+    ...use7tvChannelEmotes(channelId),
+    ...useBttvChannelEmotes(channelId),
+    ...useFfzChannelEmotes(channelId),
+    ...use7tvGlobalEmotes(),
+    ...useBttvGlobalEmotes(),
+    ...useFfzGlobalEmotes(),
+  ]
 
-	return thirdPartyEmotes;
+  return thirdPartyEmotes
 }
